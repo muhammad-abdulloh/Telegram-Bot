d9451bce6b472bd388b8df9cd5820ea9732259e5
Clever Coder <clevercoderr@gmail.com>
2021-12-22 10:22:59 +0500

    Add project files.

---
+++ Telegram Bot/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Telegram_Bot
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
